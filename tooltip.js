71e53ab1c9a0b3198c8eb25fbfec7a27cefb712f
Eugene Furmanov <codeinsack@gmail.com>
2020-04-16 00:11:59 +0300

    add attribute

--- tooltip.js
+++ tooltip.js
@@ -1,10 +1,11 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super()
-    this._tooltipContainer
+    this._tooltipContainer = null
   }
 
   connectedCallback() {
+    this._tooltipText = this.getAttribute('text')
     const tooltipIcon = document.createElement('span')
     tooltipIcon.textContent = ' (?)'
     tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this))
@@ -14,7 +15,7 @@ class Tooltip extends HTMLElement {
 
   _showTooltip() {
     this._tooltipContainer = document.createElement('div')
-    this._tooltipContainer.textContent = 'This is the tooltip text!'
+    this._tooltipContainer.textContent = this._tooltipText
     this.appendChild(this._tooltipContainer)
   }
 
