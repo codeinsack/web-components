567bef212df1f4ef0b8a94d5a40e9b17462e78b2
Eugene Furmanov <codeinsack@gmail.com>
2020-04-20 15:20:56 +0300

    remove listeners

--- tooltip.js
+++ tooltip.js
@@ -2,6 +2,7 @@ class Tooltip extends HTMLElement {
   constructor() {
     super()
     this._tooltipContainer = null
+    this._tooltipIcon = null
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.innerHTML = `
       <style>
@@ -43,10 +44,10 @@ class Tooltip extends HTMLElement {
 
   connectedCallback() {
     this._tooltipText = this.getAttribute('text')
-    const tooltipIcon = this.shadowRoot.querySelector('span')
-    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this))
-    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this))
-    this.shadowRoot.appendChild(tooltipIcon)
+    this._tooltipIcon = this.shadowRoot.querySelector('span')
+    this._tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this))
+    this._tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this))
+    this.shadowRoot.appendChild(this._tooltipIcon)
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
@@ -60,6 +61,11 @@ class Tooltip extends HTMLElement {
 
   static get observedAttributes() {
     return ['text']
+  }
+
+  disconnectedCallback() {
+    this._tooltipIcon.removeEventListener('mouseenter', this._showTooltip)
+    this._tooltipIcon.removeEventListener('mouseleave', this._hideTooltip)
   }
 
   _showTooltip() {
